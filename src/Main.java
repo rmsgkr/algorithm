e7e8d3f58a167b03b360a36f17ac6f0bfbd8ee98
Geunhak Lee <rmsgkr2@gmail.com>
2022-10-27 16:33:35 +0900

    feat: 백준 1000(입력)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
+
+/*
+굳이 String 변수 생성 안하고 입력과 동시에 구분자로 분리해줘도 된다.
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		System.out.println(a+b);
+
+*/
     }
 }
+
